27f0b88038d652232b4f48405c7725603a5f426e
root <leoman037@live.fg>
2015-11-11 19:22:01 +0100

    dr

--- install_vimrc.sh
+++ install_vimrc.sh
@@ -1,3 +1,14 @@
-curl -fLo ~/.nvim/autoload/plug.vim --create-dirs \
-https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-cp nvimrc ~/.nvimrc
+#!/bin/bash
+
+if [ "$#" -eq 1 ]; then
+	curl -fLo ~/.nvim/autoload/plug.vim --create-dirs \
+	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	cp .vimrc ~/.$1
+elif [ "$#" -eq 2 ]; then
+	curl -fLo ~/.nvim/autoload/plug.vim --create-dirs \
+	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	cp .vimrc $2/.$1
+else
+	echo "Usage is [nvim/vim]  [optional :directory]"
+fi
+
