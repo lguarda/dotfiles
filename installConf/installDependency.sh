02258de0ace527175e889808e157c4104d04aeca
lguard <leoman037@live.fr>
2019-09-23 11:34:49 +0200

    dr

--- installConf/installDependency.sh
+++ installConf/installDependency.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo apt-get -y update
-sudo apt-get -y install vim zsh git gccgo autojump curl command-not-found fuse bc ranger ncdu
+sudo apt-get -y install dtrx vim zsh git gccgo autojump curl command-not-found fuse bc ranger ncdu woof
 sudo update-command-not-found
