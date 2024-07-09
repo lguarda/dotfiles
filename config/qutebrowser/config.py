7443df29f400be589bb92f30460e4bdb1d122bd8
lguarda <leoman037@live.fr>
2024-07-09 23:09:21 +0200

    qutebrowser: add first config.py

---
+++ config/qutebrowser/config.py
@@ -0,0 +1,6 @@
+config.load_autoconfig()
+
+# Config
+c.content.blocking.adblock.lists = ['https://easylist.to/easylist/easylist.txt', 'https://easylist.to/easylist/easyprivacy.txt', 'https://easylist-downloads.adblockplus.org/easylistdutch.txt', 'https://easylist-downloads.adblockplus.org/abp-filters-anti-cv.txt', 'https://www.i-dont-care-about-cookies.eu/abp/', 'https://secure.fanboy.co.nz/fanboy-cookiemonster.txt']
+c.qt.args.append('widevine-path=/usr/lib/chromium/libwidevinecdm.so')
+c.colors.webpage.darkmode.enabled = True
