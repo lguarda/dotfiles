b154aaf6e0d8bce894b25acb1def114f4d865565
lguarda <leoman037@live.fr>
2024-07-10 17:52:38 +0200

    qutebrowser: update config with pdf view and keepassxc integration binding

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -1,6 +1,15 @@
+# {{{ Defaults
 config.load_autoconfig()
-
-# Config
+# }}}
+# {{{ Bindings
+# keepassxc integration
+config.bind('<Alt-Shift-u>', 'spawn --userscript qute-keepassxc --key F642ED23246F9615EE30AB9CA340000F06B5AD89', mode='insert')
+config.bind('pw', 'spawn --userscript qute-keepassxc --key F642ED23246F9615EE30AB9CA340000F06B5AD89', mode='normal')
+# }}}
+# {{{ settings
 c.content.blocking.adblock.lists = ['https://easylist.to/easylist/easylist.txt', 'https://easylist.to/easylist/easyprivacy.txt', 'https://easylist-downloads.adblockplus.org/easylistdutch.txt', 'https://easylist-downloads.adblockplus.org/abp-filters-anti-cv.txt', 'https://www.i-dont-care-about-cookies.eu/abp/', 'https://secure.fanboy.co.nz/fanboy-cookiemonster.txt']
+c.content.pdfjs = True # pdf viewer install libjs-pdf
 c.qt.args.append('widevine-path=/usr/lib/chromium/libwidevinecdm.so')
 c.colors.webpage.darkmode.enabled = True
+# }}}
+# vim: fdm=marker
