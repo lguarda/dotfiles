7142ca6d6ec71f1273c364645c6bba6be275f637
lguarda <leoman037@live.fr>
2024-11-05 21:08:41 +0100

    [qutebrowser] dark theme + toggle

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -5,12 +5,14 @@ config.load_autoconfig()
 # keepassxc integration
 config.bind('<Alt-Shift-u>', 'spawn --userscript qute-keepassxc --key F642ED23246F9615EE30AB9CA340000F06B5AD89', mode='insert')
 config.bind('pw', 'spawn --userscript qute-keepassxc --key F642ED23246F9615EE30AB9CA340000F06B5AD89', mode='normal')
+config.bind('<Shift-D>', 'config-cycle -u {url:domain} colors.webpage.darkmode.enabled', mode='normal')
 # }}}
 # {{{ settings
 c.content.blocking.adblock.lists = ['https://easylist.to/easylist/easylist.txt', 'https://easylist.to/easylist/easyprivacy.txt', 'https://easylist-downloads.adblockplus.org/easylistdutch.txt', 'https://easylist-downloads.adblockplus.org/abp-filters-anti-cv.txt', 'https://www.i-dont-care-about-cookies.eu/abp/', 'https://secure.fanboy.co.nz/fanboy-cookiemonster.txt']
 c.content.pdfjs = True # pdf viewer install libjs-pdf
 c.qt.args.append('widevine-path=/usr/lib/chromium/libwidevinecdm.so')
 c.colors.webpage.darkmode.enabled = False # really awful disable for now
+c.colors.webpage.preferred_color_scheme = "dark"
 c.content.autoplay = True
 # }}}
 # vim: fdm=marker
