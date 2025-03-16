2e6e0ff5e2f7bdd6b5bf5707063a287a7f9021a9
lguarda <leoman037@live.fr>
2025-03-16 22:46:13 +0100

    script: clean local script

--- local/bin/colorize.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-h=$(echo $1 | md5sum | head -c 2)
-h=$(( 40 + (16#$h % 80) * 2))
-printf "\x1b[38;5;%dm%s\x1b[0m" "$h" "$1"
--- local/bin/random_xkcd.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -sL https://c.xkcd.com/random/comic/  | perl -ne '/Image URL.*(https:\/\/.*)$/ and print "$1"'
